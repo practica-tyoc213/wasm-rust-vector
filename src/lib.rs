67c1c276cd9463d02cb49aa4beaa9bb10f5d849f
tyoc213 <tyoc213+git@tyoc.mx>
2022-01-04 23:41:48 -0600

    +Load from json +Add dot req to backend using user_id:1

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use web_sys::Request;
 use web_sys::RequestInit;
 use web_sys::Response;
 use std::f64;
-use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_futures::{JsFuture, spawn_local};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use futures::executor::block_on;
@@ -18,6 +18,8 @@ extern crate web_sys;
 
 use wasm_bindgen::prelude::*;
 
+#[macro_use]
+extern crate json;
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -33,6 +35,7 @@ macro_rules! log {
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+    fn reaload_universe_with(x: &str);
 }
 
 #[wasm_bindgen]
@@ -53,7 +56,7 @@ impl ADot {
     pub fn is_near(&self, x: u32, y: u32) -> bool {
         
         let neighboor = 7;
-        if (self.x - x as i32).abs() < neighboor || (self.y - y as i32).abs() < neighboor {
+        if (self.x - x as i32).abs() < neighboor && (self.y - y as i32).abs() < neighboor {
             // log!("is near <{}, {}>? ?? ({}, {})", self.x, self.y, x, y);
             true
         } else {
@@ -110,7 +113,7 @@ impl Universe {
         map
     }
 
-    async fn load_dots_for_user(&self, id: i32) -> Result<JsValue, JsValue>{
+    async fn load_dots_for_user(id: i32) -> Result<JsValue, JsValue>{
         let mut opts = RequestInit::new();
         opts.method("GET");
 
@@ -128,11 +131,147 @@ impl Universe {
         // `resp_value` is a `Response` object.
         assert!(resp_value.is_instance_of::<Response>());
         let resp: Response = resp_value.dyn_into().unwrap();
+        let b = resp.body_used();
 
         // Convert this other `Promise` into a rust `Future`.
-        let json = JsFuture::from(resp.json()?).await?;
-        log!("{:?}", json);
+        let json = JsFuture::from(resp.text()?).await?;
+        // let json = resp.text()?.await?;
+        // log!("segun un json: {:?}", json);
+
+        // log!("async call {}", 9);
         Ok(json)
+    }
+
+    // Ideally should serve to load asynchronously the points for the user 1
+    pub fn load(&self){
+        spawn_local(async move {
+            match Universe::load_dots_for_user(1).await {
+                Err(why) => log!("error: {:?}", why),
+                Ok(res) => {
+                    let s1 = res.as_string().unwrap();
+                    match json::parse(&s1) {
+                        Err(why) => log!("error 2: {:?}", why),
+                        Ok(jsonRes) => {
+                            log!("it is this: {} {}: {}", jsonRes["id"], jsonRes["email"], jsonRes["vectors"]);
+                            for x in 0..500 {
+                                // currently only prints them!!!
+                                log!("     {}:    {},{}", jsonRes["vectors"][x]["id"], jsonRes["vectors"][x]["x"], jsonRes["vectors"][x]["y"]);
+                            }
+                            // doesnt work: reaload_universe_with(&s1);
+                        }
+                    }
+                    
+                }
+            }
+        });
+    }
+
+    async fn add_new_point(user_id: i32, x: i32, y: i32) -> Result<JsValue, JsValue>{
+        let mut opts = RequestInit::new();
+        opts.method("POST");
+        let s = format!("{{\"x\":{},\"y\":{}}}", x, y);
+        log!("add_new_point jsons={}", s);
+        let body = JsValue::from(s);
+        opts.body(Some(&body));
+
+        let url = format!("http://localhost:8000/users/{}/vectors/", user_id);
+
+        let request = Request::new_with_str_and_init(&url, &opts)?;
+
+        request
+            .headers()
+            .set("Accept", "application/vnd.github.v3+json")?;
+
+        let window = web_sys::window().unwrap();
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+        // `resp_value` is a `Response` object.
+        assert!(resp_value.is_instance_of::<Response>());
+        let resp: Response = resp_value.dyn_into().unwrap();
+        let b = resp.body_used();
+
+        // Convert this other `Promise` into a rust `Future`.
+        let json = JsFuture::from(resp.text()?).await?;
+        // let json = resp.text()?.await?;
+        // log!("segun un json: {:?}", json);
+
+        // log!("async call {}", 9);
+        Ok(json)
+    }
+
+    // Ideally should serve to load asynchronously the points for the user 1
+    pub fn synq_add_point(&self, user_id: i32, x: i32, y: i32){
+        log!("synq_add_point");
+        spawn_local(async move {
+            match Universe::add_new_point(user_id, x, y).await {
+                Err(why) => log!("error: {:?}", why),
+                Ok(res) => {
+                    let s1 = res.as_string().unwrap();
+                    match json::parse(&s1) {
+                        Err(why) => log!("error 2: {:?}", why),
+                        Ok(json_res) => {
+                            log!("it is this: {:?}", json_res);
+                            // doesnt work: reaload_universe_with(&s1);
+                        }
+                    }
+                    
+                }
+            }
+        });
+    }
+
+
+    async fn update_point(user_id: i32, point_id:i32, x: i32, y: i32) -> Result<JsValue, JsValue>{
+        let mut opts = RequestInit::new();
+        opts.method("PUT");
+        let s = format!("{{\"id\":{},\"x\":{},\"y\":{}}}", point_id, x, y);
+        log!("update_point jsons={}", s);
+        let body = JsValue::from(s);
+        opts.body(Some(&body));
+
+        let url = format!("http://localhost:8000/users/{}/vectors/", user_id);
+
+        let request = Request::new_with_str_and_init(&url, &opts)?;
+
+        request
+            .headers()
+            .set("Accept", "application/vnd.github.v3+json")?;
+
+        let window = web_sys::window().unwrap();
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+        // `resp_value` is a `Response` object.
+        assert!(resp_value.is_instance_of::<Response>());
+        let resp: Response = resp_value.dyn_into().unwrap();
+        let b = resp.body_used();
+
+        // Convert this other `Promise` into a rust `Future`.
+        let json = JsFuture::from(resp.text()?).await?;
+        // let json = resp.text()?.await?;
+        // log!("segun un json: {:?}", json);
+
+        // log!("async call {}", 9);
+        Ok(json)
+    }
+
+    // Ideally should serve to load asynchronously the points for the user 1
+    pub fn synq_update_point(&self, user_id: i32, point_id:i32, x: i32, y: i32){
+        spawn_local(async move {
+            match Universe::update_point(user_id, point_id,x, y).await {
+                Err(why) => log!("error: {:?}", why),
+                Ok(res) => {
+                    let s1 = res.as_string().unwrap();
+                    match json::parse(&s1) {
+                        Err(why) => log!("error 2: {:?}", why),
+                        Ok(json_res) => {
+                            log!("it is this: {:?}", json_res);
+                            // doesnt work: reaload_universe_with(&s1);
+                        }
+                    }
+                    
+                }
+            }
+        });
     }
 
     pub fn new() -> Universe {
@@ -184,19 +323,30 @@ impl Universe {
         }
     }
 
-    pub fn set_mouse_down(&mut self, x: u32, y: u32) {
+    pub fn set_mouse_down(&mut self, x_in: u32, y_in: u32) {
         self.mouse_down = true;
-        let x = self.dots.iter().find(|&&d| d.is_near(x, y));
+        let x = self.dots.iter().find(|&&d| d.is_near(x_in, y_in));
         match x {
             Some(y) => {
                 self.grabing = Some(y.id)
             },
-            _ => {self.grabing = None}
+            _ => {
+                self.grabing = None;
+                log!("points: {},{}", x_in, y_in);
+                self.synq_add_point(1, x_in as i32, y_in as i32);
+                let max_id = self.dots.iter().max_by_key(|x| x.id).unwrap();
+                let p = ADot{id: (max_id.id+1) as i32, x: x_in as i32, y: y_in as i32};
+                self.dots.push(p);
+//                self.draw_dots(&canvas);
+            }
         };
     }
 
-    pub fn set_mouse_up(&mut self, _x: u32, _y: u32) {
+    pub fn set_mouse_up(&mut self, x_in: u32, y_in: u32) {
         self.mouse_down = false;
+        if let Some(point_id) = self.grabing {
+            self.synq_update_point(1, point_id as i32, x_in as i32, y_in as i32);
+        }
         self.grabing = None;
     }
 
@@ -246,6 +396,39 @@ impl Universe {
             context.fill_rect(d.x as f64, d.y  as f64, cell_size, cell_size);
         }
         context.stroke()
+    }
+
+    pub fn set_dots_now(&mut self, dots: &str) {
+        log!("passed: {}", dots);
+        let parsed = json::parse(dots);
+        log!("is parsed {:?}", parsed);
+        match parsed {
+            Err(why) => log!("error 2: {:?}", why),
+            Ok(jsonRes) => {
+                let mut l = vec![];
+                for x in 0..300 {
+                    // currently only prints them!!!
+                    log!("     {}:    {},{}", jsonRes[x]["id"], jsonRes[x]["x"], jsonRes[x]["y"]);
+                    if jsonRes[x]["id"].is_null() {
+                        log!("break {}", x);
+                        break
+                    }
+                    let jid = jsonRes[x]["id"].as_i32().unwrap();
+                    let jx = jsonRes[x]["x"].as_i32().unwrap();
+                    let jy = jsonRes[x]["y"].as_i32().unwrap();
+
+                    let a = ADot{
+                        id: jid,
+                        x: jx,
+                        y: jy,
+                    };
+                    log!("{:?}", a);
+                    l.push(a);
+                }
+                // doesnt work: reaload_universe_with(&s1);
+                self.dots = l;
+            }
+        }
     }
 }
 
